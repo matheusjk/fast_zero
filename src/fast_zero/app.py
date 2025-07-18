83cfdbec56118d8ff354eed396223b2465a89cf8
Matheus Rodrigues <matheusrodriguesh1@gmail.com>
2025-07-18 17:03:48 -0300

    Schemas | Testes | Validations | Pydantic

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,11 +1,15 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+# from fastapi.responses import HTMLResponse
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá mundo'}
+    return {'message': 'Olá mundo', 'batata': 1}
 
 
 
